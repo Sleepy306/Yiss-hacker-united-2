1bed985f221b516af1947274b362a16a9478e230
MooMooDylan <dylanxku@gmail.com>
2026-09-14 15:08:45 +0900

    Created name dialog box (not yet implemented)

--- GUI.py
+++ GUI.py
@@ -1,27 +1,66 @@
 #UI files
 
-WindowTitle = "Yiss Hackers United Network"
+import sys, threading
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, QMessageBox, QInputDialog
 
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton
+class GUI:
+    def __init__(self):
+        self.app: QApplication = None
+        self.window: QWidget = None
 
-app = QApplication(sys.argv)
-window = QWidget()
-window.setGeometry(100, 100, 800, 400)
+    def InitializeGUI(self):
+        self.app = QApplication(sys.argv)
+        self.window = QWidget()
+        self.window.setGeometry(100, 100, 800, 400)
 
-#Define name input window
-namelayout = QVBoxLayout()
+        #Define name input window
+        #self.namelayout = QVBoxLayout()
 
-window.setWindowTitle(WindowTitle)
+        #self.window.setWindowTitle("Yiss Hacker Network")
 
-inputTextField = QLineEdit(window)
-inputTextField.setPlaceholderText("Enter name...")
-namelayout.addWidget(inputTextField)
+        #self.inputTextField = QLineEdit(self.window)
+        #self.inputTextField.setPlaceholderText("Enter name...")
+        #self.namelayout.addWidget(self.inputTextField)
 
-submitButton = QPushButton("Submit", window)
-namelayout.addWidget(submitButton)
+        #self.submitButton = QPushButton("Submit", self.window)
+        #self.submitButton.clicked.connect(self.ReadName)
+        #self.namelayout.addWidget(self.submitButton)
+        #----------------------------------------------------
 
-window.setLayout(namelayout)
-window.show()
+    def ReadName(self):
+        userInput = self.inputTextField.text().strip()
+        if userInput:
+            QMessageBox.information(self.window, "Name set", f"Set name to: {userInput}")
+            return userInput
+        else:
+            QMessageBox.warning(self.window, "Name not set.", "Please reenter name")
+            return
 
-sys.exit(app.exec())
+    def NameInputPopup(self):
+        #self.window.setLayout(self.namelayout)
+        #elf.window.show()
+        name = None
+        while not name:
+            name, ok = QInputDialog.getText(None, "Enter Name", "Enter your name:")
+            if name:
+                QMessageBox.information(self.window, "Name set", f"Set name to: {name}")
+            else:
+                QMessageBox.warning(self.window, "Name not set.", "Please re-enter name")
+
+        return name
+
+
+    def CloseAppSetup(self):
+        sys.exit(self.app.exec())
+        return
+
+#Test code
+guiHandler = GUI()
+guiHandler.InitializeGUI()
+
+#guiHandler.window.setLayout(guiHandler.namelayout)
+#guiHandler.window.show()
+
+name = guiHandler.NameInputPopup()
+
+print(name)
