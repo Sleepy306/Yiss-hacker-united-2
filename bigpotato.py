00ef565a566bf38c01f8d664af9d3bb9f7cd7001
Sleepy306 <henrygamer306@gmail.com>
2026-09-16 14:07:13 +0900

    Fix merger bugs

--- bigpotato.py
+++ bigpotato.py
@@ -1,12 +1,14 @@
 import socket
 import threading
-HOST= 'localhost' #192.168.22.204'
+HOST= '192.168.22.204'
 PORT= 5000
 Soncket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 Soncket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 Soncket.bind((HOST,PORT))
 Soncket.listen(67)
 eggies={}#dictionary of users names(key) and their conection(value)
+bigger_eggies={}
+group_number=0
 print('Server is listening')
 def Alfred(con, addr):#thread that connect clients to 1-1 chat.
     while True:
@@ -25,7 +27,33 @@ def Alfred(con, addr):#thread that connect clients to 1-1 chat.
              break
          message=data
          to_who.send(message)
-        
+def Heisenburg(con,addr):
+    global group_number
+    sleepies=[]
+    while True:
+         Members=(con.recv(1024).decode('utf-8'))
+         Members=Members.split(",")
+         for users in Members:
+             if users not in eggies.keys():
+                 con.sendall((users+"does not exist").encode('utf-8'))
+         if all(users in eggies for users in Members):
+              con.sendall(("you're good to go").encode('utf-8'))
+              group_number=group_number+1
+              for users in Members:
+                  sleepies.append(eggies[users])
+              group="Group"+str(group_number)
+              bigger_eggies[group]=sleepies
+              break
+    while True:
+        message=con.recv(1024)
+        if not message:
+            con.close()
+            break
+        for users in Members:
+            if con==eggies[users]:
+                continue
+            eggies[users].sendall(message)
+         
 try:
      while True: #main thread, recieve each client's name and starts Alfred thread.
          con, addr= Soncket.accept() 
@@ -44,6 +72,12 @@ try:
 finally:
     con.close()
     Soncket.close()
+     
+     
+
+
+
+
      
      
 
