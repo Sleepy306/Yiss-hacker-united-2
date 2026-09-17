042f90ac737c9786fff26220a9f65b2462f68f65
MooMooDylan <dylanxku@gmail.com>
2026-09-17 10:07:32 +0900

    I need to commit for some reason

--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 #UI files
 
 import sys, threading
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, QMessageBox, QInputDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QVBoxLayout, QPushButton, QMessageBox, QInputDialog, QDialog
 
 class GUI:
     def __init__(self):
@@ -42,17 +42,49 @@ class GUI:
         name = None
         while not name:
             name, ok = QInputDialog.getText(None, "Enter Name", "Enter your name:")
+
+            if not ok:
+                break
+
             if name:
-                QMessageBox.information(self.window, "Name set", f"Set name to: {name}")
+                QMessageBox.information(None, "Name set", f"Set name to: {name}")
             else:
-                QMessageBox.warning(self.window, "Name not set.", "Please re-enter name")
+                QMessageBox.warning(None, "Name not set.", "Please re-enter name")
 
         return name
 
+    def ChatSelect(self):
+        groupChat = False
+        connectionName = None
+        options = ["Single Chat", "Group Chat"]
+        
+        while not connectionName:
+            groupChat, ok = QInputDialog.getItem(None, "Chat Mode", "Select chat mode:" ,options, 0, False)
 
-    def CloseAppSetup(self):
-        sys.exit(self.app.exec())
-        return
+            if not ok:
+                break
+
+            if groupChat == "Single Chat":
+                connectionName, ok = QInputDialog.getText(None, "Single Chat", "Enter user you'd like to connect to:")
+                if not connectionName:
+                    if ok:
+                        QMessageBox.warning(None, "Name not set.", "Please re-enter information")
+                    pass
+            elif groupChat == "Group Chat":
+                connectionName, ok = QInputDialog.getText(None, "Group Chat", "Enter group chat you'd like to connect to:")
+                if not connectionName:
+                    if ok:
+                        QMessageBox.warning(None, "Name not set.", "Please re-enter information")
+                    pass
+
+            if not ok:
+                break
+        
+        return connectionName
+
+#    def CloseAppSetup(self):
+#        sys.exit(self.app.exec())
+#        return
 
 #Test code
 guiHandler = GUI()
@@ -63,4 +95,7 @@ guiHandler.InitializeGUI()
 
 name = guiHandler.NameInputPopup()
 
+connection = guiHandler.ChatSelect()
+
 print(name)
+print(connection)
